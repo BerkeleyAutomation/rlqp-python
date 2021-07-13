14aa95bf0f9ed02a7ffce3b66c29d25abae48899
Paras Jain <parasj96@gmail.com>
2021-07-13 13:33:12 -0700

    pipx install

--- setup.py
+++ setup.py
@@ -259,7 +259,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='rlqp',
-      version='0.6.2.post0',
+      version='0.1',
       author='Jeff Ichnowski, Paras Jain, Bartolomeo Stellato, Goran Banjac',
       author_email='jeffi@berkeley.edu',
       description='RQLP: Accelerating Quadratic Optimization with Reinforcement Learning',
